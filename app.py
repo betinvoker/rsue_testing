02ffee2bfd8203c4b0a54c4aea01e768c86a7d1e
unknown <betinvoker1797@gmail.com>
2024-01-03 21:15:08 +0300

    Create DB and show data in page index.html

--- app.py
+++ app.py
@@ -3,19 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///db_rsue_testing.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db_rsue_testing.db"
 db = SQLAlchemy(app)
 
-
+#   Cущности базы данных
+#   Класс Пользователь 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     login = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+    role = db.Column(db.String(1), unique=False, nullable=False)
+    create_date = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
+
+#   Класс Группа
+class Group(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150), unique=False, nullable=False)
+    course = db.Column(db.String(1), unique=False, nullable=False)
+    year_study = db.Column(db.String(4), unique=False, nullable=False)
+    create_date = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
+
+#   Класс связи Пользователь-Группа (Многие ко многим)
+class UserGroup(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    id_user = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
+    id_group = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
     create_date = db.Column(db.DateTime(timezone=True), server_default=db.func.current_timestamp())
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    tests = Group.query.all()
+    return render_template('index.html', tests=tests)
 
 @app.route("/creating_tests")
 def creatingTests():
